f433dc999bc34b697d9d5423b030e3f3be225fd1
James Stewart <james@saigon.config>
2009-02-02 11:35:33 +0000

    Switch deployment to use git

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,20 +1,8 @@
 set :application, "wifi"
-#ssh_options[:username] = 'jystewart'
-set :svn_user, "deploy"
-set :svn_password, Proc.new { Capistrano::CLI.password_prompt("SVN Password for #{svn_user}: ") }
-set :repository,
-  Proc.new { "--username #{svn_user} " +
-             "--password #{svn_password} " +
-             "http://projects.jystewart.net/svn/#{application}/trunk" }
 
-# If you aren't deploying to /u/apps/#{application} on the target
-# servers (which is the default), you can specify the actual location
-# via the :deploy_to variable:
-# set :deploy_to, "/var/www/#{application}"
-
-# If you aren't using Subversion to manage your source code, specify
-# your SCM below:
-# set :scm, :subversion
+set :scm, :git
+set :repository,  "git@github.com:jystewart/grand-rapids-wifi.git"
+set :branch, "master"
 
 role :web, "grwifi.net"
 role :app, "grwifi.net"
@@ -28,23 +16,9 @@ task :restart, :roles => :web do
   "thin -C /etc/thin/wifi.thin restart"
 end
 
-desc "Configure the database"
-task :configure_database, :roles => :app do
-  set :production_database_password, proc { Capistrano::CLI.password_prompt("Production database remote Password : ") }
-  
-  db_file = YAML::load_file('config/database.yml.template')
-  # get ride of uneeded configurations
-  db_file.delete('test')
-  db_file.delete('development')
-
-  # Populate production element
-  db_file['production']['adapter'] = "mysql"
-  db_file['production']['database'] = "wifi_production"
-  db_file['production']['username'] = "jystewart"
-  db_file['production']['password'] = production_database_password
-  db_file['production']['host'] = "localhost"
-  
-  put YAML::dump(db_file), "#{release_path}/config/database.yml", :mode => 0664
+desc "Link in the production configuration"
+task :link_config do
+  run "ln -nfs #{deploy_to}/shared/config/database.yml #{release_path}/config/database.yml"
 end
 
 desc "Backup the database before we run any migrations, etc."
@@ -59,6 +33,6 @@ end
 
 desc "After updating the code"
 task :after_update, :roles => :app do
-  configure_database
+  link_config
   backup
 end
