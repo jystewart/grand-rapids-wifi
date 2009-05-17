1883bb3fd33d9c98a1fb07392c8b1891bb029e1e
James Stewart <james@saigon.local>
2009-05-17 17:46:26 +0100

    Tune that database a little more

---
+++ db/migrate/20090517162714_more_indices.rb
@@ -0,0 +1,20 @@
+class MoreIndices < ActiveRecord::Migration
+  def self.up
+    add_index :locations, :zip
+    add_index :locations, :status
+    add_index :openings, :location_id
+    add_index :openings, :opening_day
+    add_index :locations_neighbourhoods, :neighbourhood_id
+    
+    change_column :comments, :commentable_type, :string, :limit => 24
+  end
+
+  def self.down
+    change_column :comments, :commentable_type, :string
+    remove_index :locations_neighbourhoods, :neighbourhood_id
+    remove_index :openings, :opening_day
+    remove_index :openings, :location_id
+    remove_index :locations, :status
+    remove_index :locations, :zip
+  end
+end
