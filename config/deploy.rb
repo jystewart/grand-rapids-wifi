a6b9a7c82e4f5cd09dd63369357b2954baffc536
James Stewart <james@saigon.local>
2009-07-23 16:40:29 +0100

    refine deployment and add delayed_job

--- config/deploy.rb
+++ config/deploy.rb
@@ -17,10 +17,39 @@ namespace :deploy do
   end
 end
 
-desc "Link in the production configuration"
-task :link_config do
-  run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+namespace :link do
+  desc "Link in the production configuration"
+  task :config do
+    run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+  end
 end
 
-before 'deploy:migrate', "link_config"
-before 'deploy:restart', "link_config"
+# add this to config/deploy.rb
+namespace :delayed_job do
+  desc "Start delayed_job process" 
+  task :start, :roles => :app do
+    run "cd #{current_path}; RAILS_ENV=#{rails_env} script/delayed_job start" 
+  end
+
+  desc "Stop delayed_job process" 
+  task :stop, :roles => :app do
+    run "cd #{current_path}; RAILS_ENV=#{rails_env} script/delayed_job stop" 
+  end
+
+  desc "Restart delayed_job process" 
+  task :restart, :roles => :app do
+    run "cd #{current_path}; RAILS_ENV=#{rails_env} script/delayed_job restart" 
+  end
+end
+
+namespace :deploy do
+  task :restart do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+end
+
+after 'deploy:symlink', "link:config"
+after "deploy:start", "delayed_job:start" 
+after "deploy:stop", "delayed_job:stop" 
+after "deploy:restart", "delayed_job:restart"
+
