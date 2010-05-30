f0f59bae8bc28b29a69ba3398ba78342e679feaa
James Stewart <james@ketlai.co.uk>
2010-05-30 17:40:19 +0100

    Upgrade delayed_job

---
+++ db/migrate/20100530162657_add_delayed_job_index.rb
@@ -0,0 +1,9 @@
+class AddDelayedJobIndex < ActiveRecord::Migration
+  def self.up
+    add_index :delayed_jobs, [:priority, :run_at], :name => 'delayed_jobs_priority'
+  end
+
+  def self.down
+    remove_index :delayed_jobs, [:priority, :run_at], :name => 'delayed_jobs_priority'
+  end
+end
