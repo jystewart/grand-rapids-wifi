438965b626973a3aeef33c1c6d725e66719b2829
James Stewart <jys@ketlai.co.uk>
2010-12-22 20:56:00 +0000

    Hurray for Rails 3! Finally free of active-form thanks to ActiveModel

--- app/models/submitter.rb
+++ app/models/submitter.rb
@@ -1,10 +1,19 @@
-class Submitter < ActiveForm
-  attr_accessor :name, :connection, :email
-  validates_presence_of :name
-  validates_presence_of :email
-  validates_format_of :email, :with => /^[-^!$#%&'*+\/=?`{|}~.\w]+
-    @[a-zA-Z0-9]([-a-zA-Z0-9]*[a-zA-Z0-9])*
-    (\.[a-zA-Z0-9]([-a-zA-Z0-9]*[a-zA-Z0-9])*)+$/x,
-    :message => 'Please specify a valid email address'
+require 'email_validator'
 
+class Submitter
+  include ActiveModel::Validations
+  
+  attr_accessor :name, :connection, :email
+  validates :name, :presence => true
+  validates :email, :presence => true, :email => true
+
+  def initialize(args = {})
+    args.each do |k, v|
+      send("#{k}=", v)
+    end
+  end
+
+  def persisted?
+    false
+  end
 end
